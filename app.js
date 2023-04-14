cb919f7b2633e3d15267ab5fcc4a400ecd877055
Bhanu <bhanu.singh@successive.tech>
2023-04-14 12:54:40 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+
+/**
+ * 
+ * <div id="parent">
+ * 
+ *  <div id="child">
+ *    I am child
+ * </div>
+ * 
+ * <div id="child1">
+ *  I am child2
+ * </div>
+ * <h1>Hello</h1>
+ * </div>
+ * 
+ * 
+ */
+const heading = React.createElement("div", {id: 'parent'}, [React.createElement("div",
+{
+    id: 'child'
+}, "I am child"), 
+React.createElement("div",
+{
+    id: 'child1'
+}, "I am child2"),
+React.createElement("h1",
+{
+}, "hello h1")
+]);
+
+console.log('heading:', heading)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
